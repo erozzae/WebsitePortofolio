e5d4739308bb730b58b67de86d47d678f4c32741
erozzae <rosihann14@mail.ugm.ac.id>
2025-02-16 22:12:04 +0700

    added login and register api

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\API\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\HomeController;
+use App\Http\Controllers\API\RegisterController;
 
-Route::controller('auth', 'RegisterController')->group(function(){
-    Route::post('register','register');
-    Route::post('login','login');
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('/register','register');
+    Route::post('/login','login');
 });
 
 Route::middleware('auth:sanctum')->group(function(){
