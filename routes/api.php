6be6f4d8797653bc2c14bedce18c3a4c81fc2ad7
erozzae <rosihann14@mail.ugm.ac.id>
2025-02-16 21:22:11 +0700

    added several necesarry to making auth api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\API\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller('auth', 'RegisterController')->group(function(){
+    Route::post('register','register');
+    Route::post('login','login');
+});
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::resource('home',HomeController::class);
+});
